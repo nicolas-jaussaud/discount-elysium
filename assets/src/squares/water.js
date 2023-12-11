c7485a45360ed611cd0a7ac6b15a70d2d6f8ee27
Nicolas Jaussaud <nicolas@teamtangible.com>
2023-12-11 03:57:16 -0500

    Add animated texture for the water

--- assets/src/squares/water.js
+++ assets/src/squares/water.js
@@ -1,28 +1,56 @@
 import { 
   PlaneGeometry, 
-  MeshBasicMaterial,
-  TextureLoader,
-  Mesh 
+  Mesh,
+  VideoTexture,
+  MeshBasicMaterial
 } from 'three'
 
 const renderWater = ({
   coordinates,
   app,
-  scene
+  scene,
+  config = {}
 }) => {
+  /**
+   * If we load the video texture right away it might be blocked by the browser because it uses autoplay
+   */
+  app.hooks.addAction('loadComplete', () => {
 
-  const geometry = new PlaneGeometry(app.map.squareSize, app.map.squareSize)
-  const texture  = new TextureLoader().load(`./assets/ressources/world/water/water.jpg`)
-  const material = new MeshBasicMaterial({ map: texture })
-  const plane    = new Mesh(geometry, material)
+    const material = createVideoMaterial(`./assets/ressources/world/water/water-${config.animation ?? 'regular'}.mp4`)
+    
+    const geometry = new PlaneGeometry(app.map.squareSize, app.map.squareSize)
+    const plane    = new Mesh(geometry, material)
 
-  plane.position.set(
-    coordinates.x[1] - app.map.squareSize / 2, 
-    coordinates.y[1] - app.map.squareSize / 2,
-    0
-  )
+    if( config.rotation ) plane.rotation.z = config.rotation
+
+    plane.position.set(
+      coordinates.x[1] - app.map.squareSize / 2, 
+      coordinates.y[1] - app.map.squareSize / 2,
+      0
+    )
+
+    scene.add(plane)
+  })  
+}
+
+/**
+ * @see https://stackoverflow.com/a/71823411/10491705
+ */
+const createVideoMaterial = url => {
+
+  const video = document.createElement('video')
+  const source = document.createElement('source')
+
+  source.src = url
+  video.appendChild(source)
+  video.setAttribute('loop', true) 
+  video.setAttribute('muted', true) 
+  video.play()
+
+  const texture = new VideoTexture(video)
+  texture.needsUpdate = true
   
-  scene.add(plane)
+  return new MeshBasicMaterial({ map : texture })
 }
 
 export { renderWater }
--- readme.md
+++ readme.md
@@ -16,4 +16,4 @@ Ressources used in the demo:
 - Trees by [Razvan Savescu](https://www.cgtrader.com/savrazvan) from [cgtrader](https://www.cgtrader.com/free-3d-models/plant/other/blocky-trees-pack)
 - Textures:
   - Grass by [babysofja](https://www.freepik.com/author/babysofja) from [freepik](https://www.freepik.com/free-vector/seamless-textured-grass-natural-grass-pattern_11930799.htm)
-  - Water by [callmetak](https://www.freepik.com/author/callmetak) from [freepik](https://www.freepik.com/free-vector/vector-seamless-rippled-swimming-pool-abstract-illustration-horizontally-vertically-repeatable_38449309.htm#&position=2&from_view=author)
+  - Water texture created with [animation-maker](https://www.freepik.com/author/callmetak) from [brush.ninja](https://brush.ninja/create/animation-maker/)
